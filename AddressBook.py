4a87bb18db42bb2d24de5f3629409fa6013014f9
Dhingra30 <rahuldhingraajd@gmail.com>
2024-07-26 04:02:40 +0530

    All functionalities enabled -- optimized code in future commit

--- AddressBook.py
+++ AddressBook.py
@@ -1,4 +1,3 @@
-# To continue from completing edit button functionality -- mark as
 from tkinter import *
 from tkinter import messagebox
 import pandas
@@ -32,11 +31,42 @@ class AddressBookApp(Tk):
         add_button = Button(self, text="Add", command=self.on_add_button_click, height=2, width=10)
         add_button.grid(column=0, row=1, padx=10, pady=10)
 
+        view_button = Button(self, text="View", command=self.on_view_button_click, height=2, width=10)
+        view_button.grid(column=0, row=2, padx=10, pady=10)
+
         edit_button = Button(self, text="Edit", command=self.on_edit_button_click, height=2, width=10)
-        edit_button.grid(column=0, row=2, padx=10, pady=10)
+        edit_button.grid(column=0, row=3, padx=10, pady=10)
 
         delete_button = Button(self, text="Delete", command=self.on_delete_button_click, height=2, width=10)
-        delete_button.grid(column=0, row=3, padx=10, pady=10)
+        delete_button.grid(column=0, row=4, padx=10, pady=10)
+
+    def on_view_button_click(self):
+        try:
+            edit_contact, index = self.get_selected_contact()
+        except TypeError:
+            messagebox.showerror("No contact selected", "Please select a contact card to view")
+            return
+        try:
+            df = pandas.read_csv('data.csv')
+            df = df.to_dict()
+        except FileNotFoundError:
+            messagebox.showerror("Contact list empty", "There are no contacts in the address book yet")
+            return
+
+        delete_window = Toplevel(self)
+        delete_window.title("Delete Contact")
+        delete_window.configure(background=COLOR)
+        self.add_labels(delete_window)
+        self.add_entry(delete_window, should_disable=False)
+
+        for count, element in enumerate(self.entry_vars):
+            element.set(df[self.labels[count]][index])
+
+        edit = Button(delete_window, text="Edit", command=self.on_edit_button_click, height=2, width=10)
+        edit.grid(column=0, row=7, pady=20)
+
+        cancel_button = Button(delete_window, text="Cancel", command=delete_window.destroy, height=2, width=10)
+        cancel_button.grid(column=1, row=7, pady=20)
 
     def setup_contact_display(self):
 
@@ -67,7 +97,7 @@ class AddressBookApp(Tk):
         add_window.configure(background=COLOR)
 
         self.add_labels(add_window)
-        self.add_entry(add_window)
+        self.add_entry(add_window, should_disable=True)
 
         submit_button = Button(add_window, text="Submit", command=self.submit_contact, height=2, width=10)
         submit_button.grid(column=0, row=7, pady=20)
@@ -75,10 +105,12 @@ class AddressBookApp(Tk):
         cancel_button = Button(add_window, text="Cancel", command=add_window.destroy, height=2, width=10)
         cancel_button.grid(column=1, row=7, pady=20)
 
-    def add_entry(self, master):
+    def add_entry(self, master, should_disable):
         for index, var in enumerate(self.entry_vars, start=2):
             entry = Entry(master, font=FONT2, textvariable=var, width=30)
             entry.grid(column=1, row=index, padx=10, pady=10)
+            if not should_disable:
+                entry.config(state="disabled")
 
     def add_labels(self, master):
         for index, label_text in enumerate(self.labels, start=2):
@@ -110,49 +142,84 @@ class AddressBookApp(Tk):
         self.setup_contact_display()
 
     def on_edit_button_click(self):
-
-        self.get_selected_contact()
-
         try:
             edit_contact, index = self.get_selected_contact()
         except TypeError:
-            edit_contact = False
-            index = False
-        if not edit_contact:
             messagebox.showerror("No contact selected", "Please select a contact card to edit")
-        else:
-            try:
-                df = pandas.read_csv('data.csv')
-                df = df.to_dict()
-                print(df)
-                print(index)
-            except FileNotFoundError:
-                messagebox.showerror("Contact list empty", "There are no contacts in the address book yet")
-                return
+            return
+        try:
+            df = pandas.read_csv('data.csv')
+            df = df.to_dict()
+        except FileNotFoundError:
+            messagebox.showerror("Contact list empty", "There are no contacts in the address book yet")
+            return
 
-            for labels in self.labels:
-                print(df[labels][index])
+        edit_window = Toplevel(self)
+        edit_window.title("Edit Contact")
+        edit_window.configure(background=COLOR)
+        self.add_labels(edit_window)
+        self.add_entry(edit_window, should_disable=True)
 
-            edit_window = Toplevel(self)
-            edit_window.title("Edit Contact")
-            edit_window.configure(background=COLOR)
-            self.add_labels(edit_window)
-            self.add_entry(edit_window)
+        for count, element in enumerate(self.entry_vars):
+            element.set(df[self.labels[count]][index])
 
-            for count, element in enumerate(self.entry_vars):
-                element.set(df[self.labels[count]][index])
+        add_button = Button(edit_window, text="Confirm changes", command=lambda: (self.save_changes(index)),
+                            height=2,
+                            width=10)
+        add_button.grid(column=0, row=7, pady=20)
 
-            add_button = Button(edit_window, text="Confirm changes", command=lambda: (self.save_changes(index)),
-                                height=2,
-                                width=10)
-            add_button.grid(column=0, row=7, pady=20)
+        cancel_button = Button(edit_window, text="Cancel", command=edit_window.destroy, height=2, width=10)
+        cancel_button.grid(column=1, row=7, pady=20)
 
-            cancel_button = Button(edit_window, text="Cancel", height=2, width=10)
-            cancel_button.grid(column=1, row=7, pady=20)
+    def on_delete(self, index):
+        test_data = pandas.read_csv("data.csv")
+
+        test_data.drop([index], inplace=True)
+
+        test_data.to_csv("data.csv", mode='w+', index=False, header=True)
+
+        alert_box = Toplevel(self)
+        alert_box.title("Alert")
+        alert_box.configure(background=COLOR)
+        label = Label(alert_box, text="Record Deleted Successfully", background=COLOR, font=FONT)
+        label.grid(column=0, row=7, padx=10, pady=10)
+        ok_button = Button(alert_box, text="ok", command=alert_box.destroy, height=2, width=10)
+        ok_button.grid(column=0, row=8, pady=20)
+        for items in self.entry_vars:
+            items.set("")
+
+        self.setup_contact_display()
 
     def on_delete_button_click(self):
-        # Placeholder for delete functionality
-        pass
+        try:
+            edit_contact, index = self.get_selected_contact()
+        except TypeError:
+            messagebox.showerror("No contact selected", "Please select a contact card to delete")
+            return
+        try:
+            df = pandas.read_csv('data.csv')
+            df = df.to_dict()
+        except FileNotFoundError:
+            messagebox.showerror("Contact list empty", "There are no contacts in the address book yet")
+            return
+
+        delete_window = Toplevel(self)
+        delete_window.title("Delete Contact")
+        delete_window.configure(background=COLOR)
+        self.add_labels(delete_window)
+        self.add_entry(delete_window, should_disable=False)
+
+        for count, element in enumerate(self.entry_vars):
+            element.set(df[self.labels[count]][index])
+
+        delete = Button(delete_window, text="Confirm Delete", command=lambda: (self.on_delete(index)), height=2,
+                        width=10)
+        delete.grid(column=0, row=7, pady=20)
+
+        cancel_button = Button(delete_window, text="Cancel", command=delete_window.destroy, height=2, width=10)
+        cancel_button.grid(column=1, row=7, pady=20)
+
+    # messagebox.showinfo("Confirm Delete", message=f"Do you want to delete?{test_data}")
 
     def submit_contact(self):
         # Checking if the file already exists
